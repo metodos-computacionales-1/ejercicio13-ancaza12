f8a0ce1dff490b0b53efb38b121a5b8d58ae326a
Default user <ac.calderonz@uniandes.edu.co>
2019-11-10 14:58:17 +0000

    overloaded functions

--- lab13.cpp
+++ lab13.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+int operate(int a,int b)
+{
+   return (a*b);
+}
+double operate(double a, double b)
+{
+    return (a/b);
+}
 
-int main(void){
-    int ant,act,aux;
-    ant=0;
-    act=1;
-    for (int i = 0; i<10; i++){
-        std::cout<< "  " <<act<<"\n";
-        aux= act;
-        act= act + ant;
-        ant= aux;
-    }
+int main(void)
+{
+    int x=5,y=2;
+    double n=5.0,m=2.0;
+    std::cout<< operate(x,y)<<"\n";
+    std::cout<< operate(n,m)<<"\n";
     return 0;
 }
-//Este ejercicio imprime la serie fibonacci con los primeros 10 numeros (Ejercicio3)
+//Imprime 10 y 2,5. Cada funcion recibe los parametros correspondientes así tengan el mismo nombre. (overloaded functions)
 
 
