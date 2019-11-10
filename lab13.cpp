e2e3b2ece63cb1ae7be139a060c755b539cc797b
Default user <ac.calderonz@uniandes.edu.co>
2019-11-10 13:41:53 +0000

    Por referencia

--- lab13.cpp
+++ lab13.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-int subtraction(int a, int b)
+int addition(int &a, int b)
 {
     int r;
-    r=a-b;
+    r=a+b;
+    a=r;
     return r;
 }
 
 int main(void)
 {
-    int x=5,y=3,z,a,b,c;
-    z=subtraction(7,2);
-    std::cout<< "The first result is " <<&z<<"\n";
-    a=subtraction(7,2);
-    std::cout<< "The second result is " <<&a<<"\n";
-    b=subtraction(x,y);
-    std::cout<< "The third result is " <<&b<<"\n";
-    c=4+subtraction(x,y);
-    std::cout<< "The fourth result is " <<&c<<"\n";
+    int x,y,z;
+    x=5;
+    y=3;
+    z=addition(x,y);
+    std::cout<< "The result is " <<z<<"\n";
+    std::cout<< "The value of x is " <<x<<"\n";
     return 0;
 }
-//Este ejercicio imprime: The result is 0x7fffbaa1eac para los 4 casos pero si les cambio el nombre de la variable, todas ocupan un espacion de memoria diferente. (Ejercicio 1)
+//Este ejercicio imprime: The result is 8 y x es 8  (Por referencia)
