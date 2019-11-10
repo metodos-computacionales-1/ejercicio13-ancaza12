869dfe1ff0d119114c63328221055979c6b69786
Default user <ac.calderonz@uniandes.edu.co>
2019-11-10 13:18:40 +0000

    Void argument

---
+++ lab13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void print_message(void)
+{
+    std::cout<< "this is a message" <<std::endl;
+}
+
+int main(void)
+{
+    print_message();
+    return 0;
+}
+
+//Este ejercicio imprime: this is a message. (Void argument)
