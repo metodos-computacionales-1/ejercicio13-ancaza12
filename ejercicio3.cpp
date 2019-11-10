63efb1d43f6045ce21c4f15937d578ffca1ab407
Default user <ac.calderonz@uniandes.edu.co>
2019-11-10 15:13:19 +0000

    ejercicio3

---
+++ ejercicio3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void){
+    int ant,act,aux;
+    ant=0;
+    act=1;
+    for (int i = 0; i<10; i++){
+        std::cout<< "  " <<act<<"\n";
+        aux= act;
+        act= act + ant;
+        ant= aux;
+    }
+    return 0;
+}
+//Este ejercicio imprime la serie fibonacci con los primeros 10 numeros (Ejercicio3)
