21bd5ed7b37d9a272856400eb10a8551489762ba
Default user <ac.calderonz@uniandes.edu.co>
2019-11-10 13:39:34 +0000

    Ejercicio 1 diferente

--- lab13.cpp
+++ lab13.cpp
@@ -8,13 +8,15 @@ int subtraction(int a, int b)
 
 int main(void)
 {
-    int x=5,y=3,z;
+    int x=5,y=3,z,a,b,c;
     z=subtraction(7,2);
-    std::cout<< "The first result is " <<z<<"\n";
-    std::cout<< "The second result is " <<subtraction(7,2)<<"\n";
-    std::cout<< "The third result is " <<subtraction(x,y)<<"\n";
-    z=4+subtraction(x,y);
-    std::cout<< "The fourth result is " <<z<<"\n";
+    std::cout<< "The first result is " <<&z<<"\n";
+    a=subtraction(7,2);
+    std::cout<< "The second result is " <<&a<<"\n";
+    b=subtraction(x,y);
+    std::cout<< "The third result is " <<&b<<"\n";
+    c=4+subtraction(x,y);
+    std::cout<< "The fourth result is " <<&c<<"\n";
     return 0;
 }
-//Este ejercicio imprime: The result is 5,5,2,6. (Calling multiple variables)
+//Este ejercicio imprime: The result is 0x7fffbaa1eac para los 4 casos pero si les cambio el nombre de la variable, todas ocupan un espacion de memoria diferente. (Ejercicio 1)
